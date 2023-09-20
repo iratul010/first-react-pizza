d3202e2d2fcc807a0f5d10c2cd40dd7dde6cf116
Ratul Islam <iratul010@gmail.com>
2023-09-20 22:24:02 +0600

    some color,size etc...

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <div className="text-sm font-semibold">Ratul</div>;
+}
+
+export default UserName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-300 bg-yellow-500 p-4 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Ratul</p>
+      <UserName />
     </header>
   );
 }
