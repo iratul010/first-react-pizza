146196ae094a528c4f70e558f4d0465e79956385
Ratul Islam <iratul010@gmail.com>
2023-09-22 19:31:47 +0600

    redux-set ,defaultValue

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul className=" divide-y divide-stone-200 bg-red-50 px-2">
+    <ul className=" divide-y divide-stone-200  px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,6 +42,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formsErrors = useActionData();
+  const username = useSelector((state) => state.user.username);
   //my solution-"checked"
   const handlePriorityChange = (e) => {
     setPriority(e.target.checked);
@@ -54,7 +56,13 @@ function CreateOrder() {
         <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
 
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+            className="input grow"
+          />
         </div>
 
         <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
