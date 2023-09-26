3b611ddab3e5fade0b2b8a4fd48b37ebb48afdd9
Ratul Islam <iratul010@gmail.com>
2023-09-26 22:26:09 +0600

    caseReducers,update-cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,13 +3,14 @@ import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 // import PropTypes from "prop-types"; // Import PropTypes
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
   const currentQuantity = useSelector(getCurrentQuantityById(id));
   const isInCart = currentQuantity > 0;
-  console.log(currentQuantity);
+
   function handleAddToCart() {
     const newItem = {
       pizzaId: id,
@@ -41,7 +42,15 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-8 sm:gap-3">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />{" "}
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
