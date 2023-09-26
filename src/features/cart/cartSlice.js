0847987454d768c4e58a9d1e8fa6fc84e7733f9d
Ratul Islam <iratul010@gmail.com>
2023-09-26 20:37:19 +0600

    optional-chaining-addWith-2-param..

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,15 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
-  // cart: [
-  //   {
-  //     pizzaId: 12,
-  //     name: "Mediterranean",
-  //     quantity: 0,
-  //     unitPrice: 16,
-  //     totalPrice: 0,
-  //   },
-  // ],
 };
 
 const cartSlice = createSlice({
@@ -48,7 +39,12 @@ export const {
   clearCart,
 } = cartSlice.actions;
 export default cartSlice.reducer;
+export const getCart = (state) => state.cart.cart;
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, curItem) => sum + curItem.quantity, 0);
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, curItem) => sum + curItem.totalPrice, 0);
+
+// এই লাইনের মাধ্যমে একটি ফাংশন ডিফাইন করা হয়েছে যা id এবং state দুটি প্যারামিটার নেয়। এই ফাংশনের কাজ হলো ক্র্যাট করা পিজা আইডি (id) এর জন্য সংরক্ষিত পরিমাণ (quantity) প্রাপ্ত করা।যদি প্রিভিউসলি সংরক্ষিত পরিমাণ (quantity) আইটেমে থাকে তাহলে ফাংশন সেই মান (quantity) দেখাবে। যদি পরিমাণ (quantity) আইটেমে থাকে না (অর্থাৎ null বা undefined হয়) তাহলে ফাংশন 0 দেখাবে।
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
