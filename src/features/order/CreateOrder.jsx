89d8cf7274e814501e31a8cb10c38940a888174f
Ratul Islam <iratul010@gmail.com>
2023-09-28 14:44:28 +0600

    geo-location-addByToolkitWithThunks

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,14 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+
 import { useState } from "react";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
+import EmptyCart from "../cart/EmptyCart";
+import { createOrder } from "../../services/apiRestaurant";
+import store from "../../utils/store";
+import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,47 +16,26 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
-  const [priority, setPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
+  //redux call it automatically
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
   //when submit then it word change dynamically
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formsErrors = useActionData();
   const username = useSelector((state) => state.user.username);
-  //my solution-"checked"
-  const handlePriorityChange = (e) => {
-    setPriority(e.target.checked);
-  };
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className=" px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
+      <button onClick={() => dispatch(fetchAddress())}>Get Positions</button>
       {/*React-Router-Dom-{Form} <Form method="POST" action="/order/new"> */}
       <Form method="POST">
         <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
@@ -96,10 +81,9 @@ function CreateOrder() {
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             //my codes
-            checked={priority}
-            onChange={handlePriorityChange}
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            checked={withPriority}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -109,8 +93,10 @@ function CreateOrder() {
         <div>
           {/* hidden input : <div> এলিমেন্টটি আপনার ফর্মে একটি গোপন ইনপুট ফিল্ড যোগ করেছে। এই গোপন ইনপুট ফিল্ডটি ব্যবহার করে ডেটা স্টোর করা হয় যা ব্যবহারকারীর দৃষ্টিতে দেখা যায় না, কিন্তু ফর্ম জমা দেওয়া সময় সাথে পাঠানো যায়।*/}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type="primary" disabled={isSubmitting || !priority}>
-            {isSubmitting ? "Placing order..." : "Order now"}
+          <Button type="primary" disabled={isSubmitting || !withPriority}>
+            {isSubmitting
+              ? "Placing order..."
+              : `Order Now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -121,10 +107,11 @@ export async function action({ request }) {
   const formData = await request.formData();
   //convert to object
   const data = Object.fromEntries(formData);
+
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
 
   //jonas-suggetions
@@ -134,9 +121,9 @@ export async function action({ request }) {
       "Please give us your correct phone number, it might need it to contact you.";
   if (Object.keys(errors).length > 0) return errors;
   // top: if  return then under line not work
-  // const newOrder = await createOrder(order);
-  // return redirect(`/order/${newOrder.id}`);
-  return null;
+  const newOrder = await createOrder(order);
+  store.dispatch(clearCart());
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
