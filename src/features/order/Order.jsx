bb15eaeb5e79b57fec356c16ddfe498f71ee9718
Ratul Islam <iratul010@gmail.com>
2023-09-29 23:34:41 +0600

    useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -8,44 +8,54 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
-
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
+import { useEffect } from "react";
+/*
+const order = {
+  id: "ABCDEF",
+  customer: "Jonas",
+  phone: "123456789",
+  address: "Arroios, Lisbon , Portugal",
+  priority: true,
+  estimatedDelivery: "2027-04-25T10:00:00",
+  cart: [
+    {
+      pizzaId: 7,
+      name: "Napoli",
+      quantity: 3,
+      unitPrice: 16,
+      totalPrice: 48,
+    },
+    {
+      pizzaId: 5,
+      name: "Diavola",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+    {
+      pizzaId: 3,
+      name: "Romana",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+    },
+  ],
+  position: "-9.000,38.000",
+  orderPrice: 95,
+  priorityPrice: 19,
+};
+*/
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher],
+  );
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -86,8 +96,16 @@ function Order() {
         </p>
       </div>
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map((item, i) => (
-          <OrderItem item={item} key={i} />
+        {cart.map((item) => (
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            sLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
       <div className="space-y-2 bg-stone-200 px-6 py-5">
